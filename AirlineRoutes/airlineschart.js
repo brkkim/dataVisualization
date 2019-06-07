97780a265c88398c764a1ad27f205f7b43d3ff81
Brian Kim <brkkim@gmail.com>
2019-06-06 20:08:51 -0700

    Add files via upload

---
+++ AirlineRoutes/airlineschart.js
@@ -0,0 +1,98 @@
+function showData() {
+    //Get the routes from our store variable.
+    let routes = store.routes;
+
+    // Compute the number of routes per airline.
+    let airlines = groupByAirline(store.routes);
+    
+    // Draw Airlines barchart
+    drawAirlinesChart(airlines);
+}
+
+function getAirlinesChartConfig() {
+    // this sets up static demension of the SVG element
+    let width = 350;
+    let height = 400;
+    let margin = {
+        top: 10,
+        bottom: 10,
+        left: 130,  // give it enough room for y-Axis with labels.
+        right: 10
+    };
+
+    // The body is the area that will be occupied by the bars.
+    let bodyHeight = height - margin.top - margin.bottom;
+    let bodyWidth = width - margin.right - margin.left;
+
+    // The container is the SVG where we will draw the chart.
+    // In our HTML is the svg tag with the id AirlinesChart
+    let container = d3.select("#AirlinesChart");
+
+    container.attr("width", width)
+            .attr("height", height+10);
+
+    return { width, height, margin, bodyHeight, bodyWidth, container };
+}
+
+function getAirlinesChartScales(airlines, config) {
+    let { bodyWidth, bodyHeight } = config;
+    let maximumCount = d3.max(airlines.map(function(d){return d.Count;}));  // find maximum routes to determine horizontal bar graph width
+
+    let xScale = d3.scaleLinear()
+                    .range([0,bodyWidth])
+                    .domain([0,maximumCount]); 
+        //range: Set the range to go from 0 to the width of the body
+        //domain: Set the domain to go from 0 to the maximun value fount for the field 'Count'
+
+    let yScale = d3.scaleBand()  // scaleBand for ordinal values
+        .range([0, bodyHeight])
+        .domain(airlines.map(a => a.AirlineName)) //The domain is the list of ailines names
+        .padding(0.2); // padding to separate bars
+        
+    return { xScale, yScale };  // return x-axis and y-axis scales
+}
+
+function drawBarsAirlinesChart(airlines, scales, config) {
+    let {margin, container} = config; // this is equivalent to 'let margin = config.margin; let container = config.container'
+    let {xScale, yScale} = scales
+    let body = container.append("g")  // group bars
+        .style("transform", 
+          `translate(${margin.left}px,${margin.top}px)`
+        )
+  
+    let bars = body.selectAll(".bar")
+                    .data(airlines);
+        // Use the .data method to bind the airlines to the bars (elements with class bar)
+
+    //Adding a rect tag for each airline
+    bars.enter().append("rect")
+        .attr("height", yScale.bandwidth())
+        .attr("width", (d) => xScale(d.Count))
+        .attr("y", (d) => yScale(d.AirlineName))     //set the width of the bar to be proportional to the airline count using the xScale
+        .attr("fill", "#2a5599");  // this should be migrated to .css file
+  }
+
+function drawAxesAirlinesChart(airlines, scales, config){
+    let {xScale, yScale} = scales
+    let {container, margin, height} = config;
+    let axisX = d3.axisBottom(xScale)
+                  .ticks(5)
+  
+    container.append("g")  // grouping x-axis parts
+      .style("transform", 
+          `translate(${margin.left}px,${height - margin.bottom}px)`
+      )
+      .call(axisX)
+  
+    let axisY = d3.axisLeft(yScale);    // Create an axis on the left for the Y scale
+    container.append("g")  // grouping y-axis parts
+        .attr("transform","translate("+margin.left+","+margin.top+")")  // move to the right position
+        .call(axisY);    // Append a g tag to the container, translate it based on the margins and call the axisY axis to draw the left axis.
+  }
+
+function drawAirlinesChart(airlines) {
+    let config = getAirlinesChartConfig();  // setup configurations
+    let scales = getAirlinesChartScales(airlines, config); // setup scales
+    drawBarsAirlinesChart(airlines, scales, config);  // draw bars based on airline route counts
+    drawAxesAirlinesChart(airlines, scales, config);  // draw axes to show airline names and route counts
+}
---
+++ AirlineRoutes/loadData.js
@@ -0,0 +1,51 @@
+let container = d3.select("mainView"); // find main <div> tag
+let store = {};  // setup an empty dictionary for data storage
+
+// load data by reading from a .csv file
+function loadData() {
+    let promise = d3.csv("routes.csv");
+
+    return promise.then(routes => {
+        store.routes = routes;  // when file is read, store .csv into dictionary structure
+    })
+}
+
+function groupByAirline(data) {
+    // Iterate over each routes, producing a dictionary where the keys is are the airline ids and the values
+    // are the information of the airline.
+
+    // following statements create a new airline id value in the dictionary if it doesn't exist already, otherwise, it goes to next airline id value
+    let result = data.reduce((result, d) => {
+        let currentData = result[d.AirlineID] || {
+            "AirlineID": d.AirlineID,
+            "AirlineName": d.AirlineName,
+            "Count": 0
+        };
+
+        currentData.Count += 1;  // Increment the count (number of routes) of airline.
+        result[d.AirlineID] = {"AirlineID":d.AirlineID,"AirlineName":d.AirlineName, "Count": currentData.Count};  // updating the values in the dictionary
+        return result;
+    }, {})
+
+    // we use this to convert the dictionary produced by the code above,
+    // into a list, that will make it easier to create the visualization.
+    result = Object.keys(result).map(key => result[key]);
+    result = result.sort((a, b) => {
+        return d3.descending(a.Count, b.Count);
+    }) // sort the data in descending order of count.
+
+    return result;  //returning list of json objects
+}
+/* debug code to show data points
+function showData() {
+    //Get the routes from our store variable
+    let routes = store.routes;
+
+    // Compute the number of routes per airline.
+    let airlines = groupByAirline(store.routes);
+    console.log(airlines);
+}
+
+loadData().then(showData);
+*/
+
